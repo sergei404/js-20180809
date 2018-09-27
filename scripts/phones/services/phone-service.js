5a60e8119c092a79b66fa599985ca6719d9940b5
Stanislav Gavrylov <stasgavrylov@gmail.com>
2018-09-27 20:58:47 +0300

    Adds delayed callbacks

--- scripts/phones/services/phone-service.js
+++ scripts/phones/services/phone-service.js
@@ -6,27 +6,35 @@ const PhoneService = {
   },
 
   getPhone(phoneId, callback) {
-    // HttpService.sendRequest(`/${phoneId}.json`,  {
-    //   errorCallback: () => {},
-    //   successCallback: callback,
-    // });
+    // HttpService.sendRequest(`/${phoneId}.json`,  callback);
+
     let promise = this._sendRequest(`/${phoneId}.json`);
     promise.then(callback)
-    promise.then((result) => {
-      console.log(result);
-    })
+    setTimeout(() => {
+      promise.then((result) => {
+        console.log(result);
+      })
+    }, 1000);
   },
 
   _sendRequest(url) {
     let promise = {
       _successCallbacks: [],
+      _status: 'pending',
+      _result: null,
 
       _resolve(data) {
+        this._status = 'fulfilled';
+        this._result = data;
         this._successCallbacks.forEach(callback => callback(data));
       },
 
       then(successCallback) {
-        this._successCallbacks.push(successCallback);
+        if (this._status === 'fulfilled') {
+          successCallback(this._result);
+        } else {
+          this._successCallbacks.push(successCallback);
+        }
       }
     }
 
