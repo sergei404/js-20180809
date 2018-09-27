b4034208e5d919295ff79635bbf169b00e7f33d8
Stanislav Gavrylov <stasgavrylov@gmail.com>
2018-09-27 21:32:59 +0300

    Adds error handling with catch

--- scripts/common/services/http-service.js
+++ scripts/common/services/http-service.js
@@ -1,17 +1,22 @@
 const API_URL = 'http://localhost:3000/phones';
 
 const HttpService = {
-  sendRequest(url, successCallback) {
+  sendRequest(url, successCallback, errorCallback) {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', API_URL + url, true);
     xhr.send();
 
     xhr.onload = () => {
+      if (xhr.status !== 200) {
+        errorCallback(new Error('Error'));
+        return;
+      }
+
       let responseData = JSON.parse( xhr.responseText );
       successCallback(responseData);
     }
 
-    // xhr.onerror = errorCallback;
+    xhr.onerror = errorCallback;
   }
 }
 
--- scripts/phones/services/phone-service.js
+++ scripts/phones/services/phone-service.js
@@ -1,5 +1,50 @@
 import HttpService from '../../common/services/http-service.js';
 
+class MyPromise {
+  constructor(behaviorFunction) {
+    this._successCallbacks = [];
+    this._errorCallbacks = [];
+    this._status = 'pending';
+    this._result = null;
+
+    behaviorFunction(this._resolve.bind(this), this._reject.bind(this));
+  }
+
+  _resolve(data) {
+    this._status = 'fulfilled';
+    this._result = data;
+    this._successCallbacks.forEach(callback => callback(data));
+  }
+
+  then(successCallback, errorCallback = () => {}) {
+    if (this._status === 'fulfilled') {
+      successCallback(this._result);
+    } else if (this._status === 'rejected') {
+      errorCallback(this._result);
+    } else {
+      this._successCallbacks.push(successCallback);
+      this._errorCallbacks.push(errorCallback);
+    }
+  }
+
+  catch(errorCallback) {
+    if (this._status === 'rejected') {
+      errorCallback(this._result);
+    } else {
+      this._errorCallbacks.push(errorCallback);
+    }
+  }
+
+  _reject(error) {
+    this._status = 'rejected';
+    this._result = error;
+
+    this._errorCallbacks.forEach(callback => {
+      callback(error);
+    })
+  }
+}
+
 const PhoneService = {
   getPhones(callback) {
     HttpService.sendRequest('/phones.json', callback);
@@ -9,7 +54,8 @@ const PhoneService = {
     // HttpService.sendRequest(`/${phoneId}.json`,  callback);
 
     let promise = this._sendRequest(`/${phoneId}.json`);
-    promise.then(callback)
+    promise.then(callback, (error) => { console.error(error); });
+    promise.catch((error) => { console.log('catched error:', error); });
     setTimeout(() => {
       promise.then((result) => {
         console.log(result);
@@ -18,28 +64,8 @@ const PhoneService = {
   },
 
   _sendRequest(url) {
-    let promise = {
-      _successCallbacks: [],
-      _status: 'pending',
-      _result: null,
-
-      _resolve(data) {
-        this._status = 'fulfilled';
-        this._result = data;
-        this._successCallbacks.forEach(callback => callback(data));
-      },
-
-      then(successCallback) {
-        if (this._status === 'fulfilled') {
-          successCallback(this._result);
-        } else {
-          this._successCallbacks.push(successCallback);
-        }
-      }
-    }
-
-    HttpService.sendRequest(url, (data) => {
-      promise._resolve(data);
+    let promise = new MyPromise((resolve, reject) => {
+      HttpService.sendRequest(url, resolve, reject);
     });
 
     return promise;
